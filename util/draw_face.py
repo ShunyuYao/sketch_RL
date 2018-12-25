5acd0f6ed75b7d2b05b075cbec38992aac49567d
ShunyuYao <ysy2017@sjtu.edu.cn>
2018-12-26 00:58:40 +0800

    test_edge

---
+++ util/draw_face.py
@@ -0,0 +1,106 @@
+import os.path
+# import torchvision.transforms as transforms
+# import torch
+from PIL import Image
+import numpy as np
+import cv2
+from skimage import feature
+from keypoint2img import interpPoints, drawEdge
+
+NoCannyEdge = False
+
+def get_face_image(A_path, size, img, transform_A=None, transform_L=None):
+    # read face keypoints from path and crop face region
+    keypoints, part_list, part_labels = read_keypoints(A_path, size)
+
+    # draw edges and possibly add distance transform maps
+    # add_dist_map = not self.opt.no_dist_map
+    im_edges = draw_face_edges(keypoints, part_list, transform_A, size, None)
+
+    # canny edge for background
+    if not NoCannyEdge:
+        edges = feature.canny(np.array(img.convert('L')))
+        edges = edges * (part_labels == 0)  # remove edges within face
+        im_edges += (edges * 255).astype(np.uint8)
+    # edge_tensor = transform_A(Image.fromarray(self.crop(im_edges)))
+
+    # final input tensor
+    # input_tensor = torch.cat([edge_tensor, dist_tensor]) if add_dist_map else edge_tensor
+    # label_tensor = transform_L(Image.fromarray(self.crop(part_labels.astype(np.uint8)))) * 255.0
+    # return input_tensor, label_tensor
+    return im_edges
+
+def read_keypoints(A_path, size):
+    # mapping from keypoints to face part
+    part_list = [[list(range(0, 17)) + list(range(68, 83)) + [0]], # face
+                 [range(17, 22)],                                  # right eyebrow
+                 [range(22, 27)],                                  # left eyebrow
+                 [[28, 31], range(31, 36), [35, 28]],              # nose
+                 [[36,37,38,39], [39,40,41,36]],                   # right eye
+                 [[42,43,44,45], [45,46,47,42]],                   # left eye
+                 [range(48, 55), [54,55,56,57,58,59,48]],          # mouth
+                 [range(60, 65), [64,65,66,67,60]]                 # tongue
+                ]
+    label_list = [1, 2, 2, 3, 4, 4, 5, 6] # labeling for different facial parts
+    keypoints = np.loadtxt(A_path, delimiter=',')
+
+    # add upper half face by symmetry
+    pts = keypoints[:17, :].astype(np.int32)
+    baseline_y = (pts[0,1] + pts[-1,1]) / 2
+    upper_pts = pts[1:-1,:].copy()
+    upper_pts[:,1] = baseline_y + (baseline_y-upper_pts[:,1]) * 2 // 3
+    keypoints = np.vstack((keypoints, upper_pts[::-1,:]))
+
+    # label map for facial part
+    w, h = size
+    part_labels = np.zeros((h, w), np.uint8)
+    for p, edge_list in enumerate(part_list):
+        indices = [item for sublist in edge_list for item in sublist]
+        pts = keypoints[indices, :].astype(np.int32)
+        cv2.fillPoly(part_labels, pts=[pts], color=label_list[p])
+
+    # move the keypoints a bit
+    # if not self.opt.isTrain and self.opt.random_scale_points:
+    #     self.scale_points(keypoints, part_list[1] + part_list[2], 1, sym=True)
+    #     self.scale_points(keypoints, part_list[4] + part_list[5], 3, sym=True)
+    #     for i, part in enumerate(part_list):
+    #         self.scale_points(keypoints, part, label_list[i]-1)
+
+    return keypoints, part_list, part_labels
+
+def draw_face_edges(keypoints, part_list, transform_A, size, add_dist_map):
+    w, h = size
+    edge_len = 3  # interpolate 3 keypoints to form a curve when drawing edges
+    # edge map for face region from keypoints
+    im_edges = np.zeros((h, w), np.uint8) # edge map for all edges
+    # dist_tensor = 0
+    e = 1
+    for edge_list in part_list:
+        for edge in edge_list:
+            im_edge = np.zeros((h, w), np.uint8) # edge map for the current edge
+            for i in range(0, max(1, len(edge)-1), edge_len-1): # divide a long edge into multiple small edges when drawing
+                sub_edge = edge[i:i+edge_len]
+                x = keypoints[sub_edge, 0]
+                y = keypoints[sub_edge, 1]
+
+                curve_x, curve_y = interpPoints(x, y) # interp keypoints to get the curve shape
+                drawEdge(im_edges, curve_x, curve_y)
+                # if add_dist_map:
+                #     drawEdge(im_edge, curve_x, curve_y)
+
+            # if add_dist_map: # add distance transform map on each facial part
+            #     im_dist = cv2.distanceTransform(255-im_edge, cv2.DIST_L1, 3)
+            #     im_dist = np.clip((im_dist / 3), 0, 255).astype(np.uint8)
+            #     im_dist = Image.fromarray(im_dist)
+            #     tensor_cropped = transform_A(self.crop(im_dist))
+            #     dist_tensor = tensor_cropped if e == 1 else torch.cat([dist_tensor, tensor_cropped])
+            #     e += 1
+
+    return im_edges #, dist_tensor
+
+if __name__ == '__main__':
+    A_path = '/home/yaosy/Diskb/research300/videoSegData/mmi/mmif_edge/train_keypoints/S002-076/00001.txt'
+    img = Image.open('/home/yaosy/Diskb/research300/videoSegData/mmi/mmif/frame/S002-076/00001.jpg')
+    size = img.size
+
+    Image.fromarray(get_face_image(A_path, size, img)).save('../test/face_test.png')
--- util/face_landmark_detection.py
+++ util/face_landmark_detection.py
@@ -5,22 +5,26 @@ import numpy as np
 import dlib
 import sys
 
-def face_landmark_detection(dataset_path, predictor_path, save_path, phase='train'):
+def face_landmark_detection(dataset_path, predictor_path, dst_path, phase='train'):
 
     faces_folder_path = dataset_path #os.path.join(dataset_path, phase + '_img/')
     #predictor_path = os.path.join(dataset_path, 'shape_predictor_68_face_landmarks.dat')
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(predictor_path)
 
-    img_paths = sorted(glob.glob(faces_folder_path + '*'))
+    img_paths = sorted(glob.glob(faces_folder_path + "/*"))
+
     for i in range(len(img_paths)):
+        if i > 2:
+            break
+
         f = img_paths[i]
         print("Processing video: {}".format(f))
-        save_path = os.path.join(save_path, phase + '_keypoints', os.path.basename(f))
+        save_path = os.path.join(dst_path, phase + '_keypoints', os.path.basename(f))
         if not os.path.isdir(save_path):
             os.makedirs(save_path)
 
-        for img_name in sorted(glob.glob(os.path.join(f, '*.bmp'))):
+        for img_name in sorted(glob.glob(os.path.join(f, '*.jpg'))):
             img = io.imread(img_name)
             dets = detector(img, 1)
             if len(dets) > 0:
@@ -32,3 +36,9 @@ def face_landmark_detection(dataset_path, predictor_path, save_path, phase='trai
 
                 save_name = os.path.join(save_path, os.path.basename(img_name)[:-4] + '.txt')
                 np.savetxt(save_name, points, fmt='%d', delimiter=',')
+
+if __name__ == '__main__':
+    dataset_path = '/home/yaosy/Diskb/research300/videoSegData/mmi/mmif/frame'
+    predictor_path = '/home/yaosy/Diskb/research300/sketch_rl/sketch_RL/util/face_extract/shape_predictor_68_face_landmarks.dat'
+    save_path = '/home/yaosy/Diskb/research300/videoSegData/mmi/mmif_edge'
+    face_landmark_detection(dataset_path, predictor_path, save_path)
